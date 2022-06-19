a551b7f13a70bc11b39d553539cbaf61bbf75890
Sasha Riaboshapka <sashariaboshapka2003@gmail.com>
2022-06-19 03:31:06 +0300

    refactor: unused imports

--- app/src/main/java/com/example/myfragmentsapp/ui/main/ButtonsFragment.kt
+++ app/src/main/java/com/example/myfragmentsapp/ui/main/ButtonsFragment.kt
@@ -1,13 +1,11 @@
 package com.example.myfragmentsapp.ui.main
 
-import android.graphics.Color
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.myfragmentsapp.R
 
--- app/src/main/java/com/example/myfragmentsapp/ui/main/ColouredFragment2.kt
+++ app/src/main/java/com/example/myfragmentsapp/ui/main/ColouredFragment2.kt
@@ -1,14 +1,13 @@
 package com.example.myfragmentsapp.ui.main
 
-import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import com.example.myfragmentsapp.R
